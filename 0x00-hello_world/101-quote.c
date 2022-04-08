5864984d887895a684261ede2fe874c0728c0e45
Charles Enahoro <enahoroomonkhobhioriacharles@gmail.com>
2022-04-07 18:43:04 -0700

    ikk

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
 /**
  * main - Entry point
  *
- * Return: Alway 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19|n9\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
