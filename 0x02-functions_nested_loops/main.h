e824ae5f385018a209c91cc7405993811b5b9e64
Charles Enahoro <enahoroomonkhobhioriacharles@gmail.com>
2022-04-12 16:10:21 -0700

    square Update

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int c);
+int print_last_digit(int n);
+#endif
